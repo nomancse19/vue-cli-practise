ccc4f72cf36793d6400fd1bf457478d1aebc10b8
Md. Jahidul Islam Noman <55144234+nomancse19@users.noreply.github.com>
2022-09-05 21:25:00 +0600

    new Componenet add

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <HelloWorld msg="Welcome To My Vue Js Site"></HelloWorld>
   <Welcome />
-  <Welcome />
-  <Welcome />
-  <Welcome />
+  <new-data />
 </template>
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import Welcome from "./components/WelCome.vue";
+import NewData from "./components/NewData.vue";
 export default {
   name: "App",
   components: {
     Welcome,
     HelloWorld,
+    NewData,
   },
 };
 </script>
---
+++ src/components/NewData.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="main">
+      <h1>Hello I Am From Vue JS New Template</h1>
+      <p>Data MSG..{{ msg }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello Viewe I am From Component",
+    };
+  },
+};
+</script>
+
+<style></style>
