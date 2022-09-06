b6d593c876006608fcceae5d06151171eceee264
Md. Jahidul Islam Noman <55144234+nomancse19@users.noreply.github.com>
2022-09-06 13:02:13 +0600

    Add Component Custom Property With Dynamic

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <HelloWorld msg="Welcome To My Vue Js Site"></HelloWorld>
+  <CustomComponent
+    age="27"
+    name="Jahidul Islam Noman"
+    department="CSE"
+    skill="PHP, Laravel,Zend Certified"
+  ></CustomComponent>
+  <CustomComponent
+    age="25"
+    name="Test Purpose Name"
+    department="Non CSE"
+    skill="No Skills Available"
+  ></CustomComponent>
 </template>
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import CustomComponent from "./components/CustomComponent.vue";
 export default {
   name: "App",
   components: {
     HelloWorld,
+    CustomComponent,
   },
 };
 </script>
